bd4658e5bd31fe280b0b951c911847ebbe68b5ad
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-03 00:58:05 -0800

    fixed bug on main.cpp so user can enter input with multiple words

--- BullCowGame/BullCowGame/main.cpp
+++ BullCowGame/BullCowGame/main.cpp
@@ -14,8 +14,13 @@ int main() {
 	// get a guess from the player
 	cout << "Enter your guess: ";
 	string Guess = "";
-	cin >> Guess;
+	getline(cin, Guess);
+	// repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
 
+	// get a guess from the player
+	cout << "Enter your guess: ";
+	getline(cin, Guess);
 	// repeat the guess back to them
 	cout << "Your guess was: " << Guess << endl;
 
