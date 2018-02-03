959e0bc10f6d1d0452f6cf90809aa4dc2197874e
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-02 17:45:42 -0800

    pseudo coding next pieces of the program

--- BullCowGame/BullCowGame/main.cpp
+++ BullCowGame/BullCowGame/main.cpp
@@ -3,9 +3,15 @@
 using namespace std;
 
 int main() {
+	// introduce the game
 	constexpr int WORD_LENGTH = 5;
-	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?\n";
+
+	// get a guess from the player
+
+	// repeat the guess back to them
+
 	return 0;
 }
