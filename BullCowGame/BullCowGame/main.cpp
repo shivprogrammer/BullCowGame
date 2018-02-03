c37f9b5d4ef87b64ec37ee23b4ff355a8251c3ba
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-02 17:59:38 -0800

    more updates to main.cpp, console asks and reads back user guess

--- BullCowGame/BullCowGame/main.cpp
+++ BullCowGame/BullCowGame/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -8,10 +9,16 @@ int main() {
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?\n";
+	cout << endl;
 
 	// get a guess from the player
+	cout << "Enter your guess: ";
+	string Guess = "";
+	cin >> Guess;
 
 	// repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
 
+	cout << endl;
 	return 0;
 }
